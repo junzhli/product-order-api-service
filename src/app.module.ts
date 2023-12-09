380229ae96f25a0c2630a5d6773767fc7ec15f6f
Jeremy Li <junzhli@gmail.com>
2023-12-09 23:44:35 +0800

    Update root modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,45 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { Dialect } from 'sequelize';
+import { Role } from './models/role';
+import { User } from './models/user';
+import { Product } from './models/product';
+import { Order } from './models/order';
+import { OrderProduct } from './models/order-product';
+import { ProductController } from './controller/product.controller';
+import { ProductService } from './service/product.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot(
+      { isGlobal: true }
+    ),
+    SequelizeModule.forRoot({
+      dialect: process.env.DB_DIALECT as Dialect,
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT, 10),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      models: [
+        Role,
+        User,
+        Product,
+        Order,
+        OrderProduct,
+      ],
+      synchronize: false,
+    }),
+    SequelizeModule.forFeature([
+      Role,
+      User,
+      Product,
+      Order,
+      OrderProduct,
+    ]),
+  ],
+  controllers: [ProductController],
+  providers: [ProductService],
 })
 export class AppModule {}
