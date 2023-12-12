571142dd4d2ad5165a556daad1a20442767f0c1d
Jeremy Li <junzhli@gmail.com>
2023-12-13 00:12:07 +0800

    Add index in product table

---
+++ migrations/20231212150523-add-index-at-column-price-and-stock-in-product-table.js
@@ -0,0 +1,24 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    await queryInterface.sequelize.query(`CREATE INDEX CONCURRENTLY IF NOT EXISTS "idx_price" ON "Products" USING btree ("price")`);
+    await queryInterface.sequelize.query(`CREATE INDEX CONCURRENTLY IF NOT EXISTS "idx_stock" ON "Products" USING btree ("stock")`);
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  }
+};
