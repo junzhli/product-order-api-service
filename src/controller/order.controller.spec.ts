e47a12de5acf2d8286d52a913a014015fd4484e6
Jeremy Li <junzhli@gmail.com>
2023-12-13 09:28:46 +0800

    Add tests

---
+++ src/controller/order.controller.spec.ts
@@ -0,0 +1,54 @@
+
+import { getModelToken } from "@nestjs/sequelize";
+import { OrderController } from './order.controller';
+import { OrderService } from '../service/order.service';
+import { Test, TestingModule } from "@nestjs/testing";
+import { Order } from "../models/order";
+import { Product } from "../models/product";
+import { OrderProduct } from "../models/order-product";
+import { mockCustomorUserJwtContent, mockOrderCreationDto } from "../../test/mock";
+import { Sequelize } from "sequelize-typescript";
+
+describe('orderController', () => {
+  let orderController: OrderController;
+  let orderService: OrderService;
+
+  const mockSequelizeOrders = {
+    findAll: jest.fn(),
+  };
+
+  const mockSequelizeProducts = {
+    findAll: jest.fn(),
+  };
+
+  const mockSequelizeOrderProducts = {
+    findAll: jest.fn(),
+  };
+
+  const mockSequelize = {
+    transaction: jest.fn(),
+  }
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+        providers: [
+            OrderService,
+          { provide: getModelToken(Order), useValue: mockSequelizeOrders },
+          { provide: getModelToken(Product), useValue: mockSequelizeProducts },
+          { provide: getModelToken(OrderProduct), useValue: mockSequelizeOrderProducts },
+          { provide: Sequelize, useValue: mockSequelize },
+        ],
+      }).compile();
+    orderService = module.get<OrderService>(OrderService);
+    orderController = new OrderController(orderService);
+  });
+
+  describe('createOrder', () => {
+    it('should return nothing when success', async () => {
+      const result = undefined;
+      jest.spyOn(orderService, "createOrder").mockImplementation(() => result);
+
+      expect(await orderController.createOrder(mockOrderCreationDto, mockCustomorUserJwtContent)).toBe(result);
+    });
+  });
+});
---
+++ test/mock.ts
@@ -0,0 +1,15 @@
+import { OrderCreationDto } from "../src/dto/order.dto";
+import { Role } from "../src/decorator/role.decorator";
+import { IAuthJwtTokenContent } from "../src/interfaces/auth.interface";
+
+export const mockCustomorUserJwtContent: IAuthJwtTokenContent = {
+    id: "1",
+    roleId: Role.Customer,
+}
+
+export const mockOrderCreationDto: OrderCreationDto = {
+    orderProducts: [{
+      id: "1",
+      count: 2,
+    }],
+};
